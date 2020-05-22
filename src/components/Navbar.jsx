b64bebb2ecade5ff40bb8d78b4092c8512ba1fc4
guicoma <guilem.coma@blackdata.systems>
2020-05-22 12:52:38 +0200

    Remove useState

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TweenOne from 'rc-tween-one';
 import { Menu } from 'antd';
 import { getChildrenToRender } from '../utils';
